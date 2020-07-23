13fbf6966a4cf99edaecb742454bc5188cb83f18
MuhammadUmair321 <61585097+MuhammadUmair321@users.noreply.github.com>
2020-07-23 07:08:40 -0700

    Add files via upload
    js asimnet

---
+++ app.js
@@ -0,0 +1,904 @@
+
+/*chep01 to 20
+Q1
+alert("Well come to my Site");
+Q2alert("Erro! please enter a valid password");
+Q3alert("Wellcom to js land...\nhappy coding!");
+
+
+Chep 02
+Q1
+var userName ="";
+var MyName ="joen doe";
+var Msg =  "Hello world";
+alert(Msg);
+Q2
+var StudName="Muhammad Umair";
+alert(StudName);
+Q3
+var FatehrName="Adul Kahliq";
+alert(FatehrName);
+var age ="30 years";
+alert(age);
+Q4
+var Certificate ="certifit Mobile and web application";
+alert(Certificate);
+Q5
+var pizzzz ="PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(pizzzz);
+Q6
+var Email ="My  Email Adress is @exmple.com";
+alert(Email);
+Q7
+var book ="A smarter to way learn JavaSeript";
+alert(+"i am trying to learn form the book"+ book);
+Q8
+ var ref="yah! I can reight HTML content through JavaSecript";
+
+ document.write(ref);
+
+ var draw ="=========%%%%%%%==============";
+ alert(draw);
+ 
+ CHP3
+  Q1var age ="I am 15 years old.";
+ alert(age);
+ Q2var vist="You have visited this sit 14 times.";
+ alert(vist);
+ 
+ Q3var birtDate="My Birth date is 1989/nData type of my ddeclared varibale is number";
+ document.write(birtDate);
+ 
+ Q4var visitor ="Doe Orderd 5 TShirt(s) on XYZ clothing store.";
+document.write(visitor);
+
+Chep4
+
+
+
+var decaler1="varibale1";
+ var decaler1="variable2";
+ var decaler1="variable3";
+ var heading="Ruls for naming JS"+"<br>"+"<br>";
+ var rule1="Varibale name can anly be  contain $and_ for exmple_istname"+"<br>";
+ var rule2="Variable must be began with a letter $or_ for exmple :$name_name or name"+"<br>";
+ var rule3="variable name are case sansative."+"<br>";
+ var rule4="variable name Shoud not be js keyword";
+ 
+ document.write(heading);
+ document.write(rule1);
+ document.write(rule2);
+ document.write(rule3);
+ document.write(rule4);
+
+chp5
+
+var num1=+"3";
+var num2=+"5";
+var ans = num1+num2;
+document.write("Sum of 3 and 5 is=",  ans+"<br>");
+
+
+var num1=+"5";
+var num2=+"3";
+var ans = num1-num2;
+document.write("Subraction of 5 and 3 is=",  ans+"<br>"+"<br>");
+
+var first="Value after variable declaration is undifined"+"<br>";
+var text="initial value:"+"&nbsp";
+var num=+"5"+"<br>";
+var second="value after increment is:"+"&nbsp";
+var nu1=+"1";
+var nu2=+"5";
+var res = nu1+nu2+"<br>";
+var nu3=+"7";
+var nu4=+"6";
+var ress=nu3+nu4+"<br>";
+var second="value after decrment is:"+"&nbsp";
+var nu5=+"13";
+var nu6=+"1";
+var res2=nu5-nu6+"<br>";
+var rem="Reminder is :0"+"<br>";
+var movie="Total cost to 5 tickets to a movie is";
+var movieper=+"600";
+var movie2=+"5";
+var moivet=movieper*movie2;
+var pkk="PKR"+"<BR>";
+document.write(first,text,num,second,res,second,ress,second,res2,rem,movie,moivet,pkk);
+
+chp6
+to9
+
+
+/*var texy ="Reasult"+"<br>"+"The value of A :10"+"<br>"+"............................"+"<br>";
+var num1 = +"10";
+var num2=++num1+"<br>";
+var tex="The value of ++a is";
+var tex1="Now the value of a is";
+var num3=+"11"+"<br>"+"<br>";
+var text4="The value of a++ is 11"+"<br>";
+var text2="The value of a is:";
+var num4=+"11";
+var num5=+"1";
+var rest=num4+num5+"<br>"+"<br>";
+var text5="The value of a--is 11"+"<br>"+"Now the value of a is";
+var num6=+"11";
+var num7=+"1";
+var rest2=num6-num7+"<br>"+"<br>";
+document.write(texy,tex,num2,tex1,num3,text4,text2,rest,text5,rest2);
+
+
+var a=2,b=1;
+var result = --a- --b+ ++b+b--+"<br>";
+document.write(result);
+
+var rsult1 =--a+"<br>";
+document.write(rsult1);
+var result2=--a- --b+"<br>";
+document.write(result2);
+var result3=--a - --b+ ++b+"<br>";
+document.write(result3);
+var resullt4=--a- --b+ ++b+b--+"<br>";
+document.write(resullt4);
+
+var User= prompt('Entner your Name')
+
+alert("Well come"+" "+User);
+
+
+
+var user1 = + prompt('Eenter any Number')
+var user2 = +"5";
+var result5 = user1*"5";    
+if(user1 === 0){
+     
+document.write("Defult value is "+" "+user2+"<br>"+"<br>");
+
+
+}else{
+
+document.write("your number after multipilication"+" "+result5+"<br>"+"<br>");
+}
+
+
+var subject1 = prompt('Enter Subject Number 01');
+var subject2 = prompt('Enter Subject Number 02');
+var subject3 = prompt('Enter Subject Number 03');
+var  perjubject=+"100";
+var totalmarsk = +'300';
+
+var obtainedmarsksub1 = +prompt('Enter obtained Marsk subject number 01');
+var obtainedmarsksub2 = +prompt('Enter obtained Marsk subject number 02');
+var obtainedmarsksub3 = +prompt('Enter obtained Marsk subject number 03');
+var single = obtainedmarsksub1/totalmarsk*+"100";
+var single1 = obtainedmarsksub2/totalmarsk*+"100";
+var single2 = obtainedmarsksub3/totalmarsk*+"100";
+var totalobtain = obtainedmarsksub1 + obtainedmarsksub2+ obtainedmarsksub3;
+var divde = totalobtain/totalmarsk*+"100";
+document.write("Subject"+" "+"Total Marsk"+" "+"Obtained Marsk"+" "+"Persantage"+"<br>");
+document.write(subject1+"&nbsp&nbsp&nbsp&nbsp&nbsp"+perjubject+"&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp " + obtainedmarsksub1+" &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp"+ single+"<br>");
+document.write(subject2+"&nbsp&nbsp&nbsp&nbsp&nbsp"+perjubject+"&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp " + obtainedmarsksub2+" &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp"+ single1+"<br>");
+document.write(subject3+"&nbsp&nbsp&nbsp&nbsp&nbsp"+perjubject+"&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp " + obtainedmarsksub3+" &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp"+ single2+"<br>");
+document.write("&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp"+totalmarsk+"&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp"+totalobtain+"&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp"+divde+"<br>");
+
+
+var city = prompt('Enter City');
+
+if(city ==="Karachi"){
+
+document.write("WELL COME CITY OF LIGHT"+"<br>");
+
+
+}
+
+var gender=prompt('Enter your Gender');
+
+if(gender === "Male"){
+
+document.write("Good Morning Sir");
+
+}else{
+
+document.write("Good Morning Ma`am"+"<br>"+"<br>");
+
+}
+
+
+var Single=prompt("Enter colour");
+var red ="Red";
+var yellow = "Yellow";
+var green = "Green";
+
+if(Single ==="Red"){
+
+        alert("You Must Stop");
+    }
+    
+    else if(Single === "Yellow"){
+        alert("Ready To Move");
+    }else{if(Single === "Green"){alert("Move Now")}}
+    
+ 
+    
+
+var renainingful = + prompt('Enter Fuel');
+var lessthan = +"0.25";
+if(renainingful  < +0.25){
+
+alert("“Please refill the fuel in your car”");
+
+}
+
+var a =+ 4;
+if(++a === 5)
+{
+
+    alert("Given condition is Ture");
+}
+
+var b=+82;
+if(b++ === 83){
+
+alert("Given condition Is true");
+
+}var c=+12
+
+if(c++ === 13){
+
+alert("Condition 1 is true");
+
+}
+
+if(c === 13){
+
+alert("Condition 2 is true");
+
+}if(c++<14){
+
+alert("Condition 3 is true");
+
+}
+
+if(c ===14){
+
+    alert("Condition 4 is true")
+}
+
+var materialCost = 20000;
+var laborCost = 2000;
+var totalCost = materialCost + laborCost;
+if (totalCost === laborCost + materialCost){
+alert("Condition TRUE");
+}else{
+
+    alert("Condition is fals");
+}
+
+if("car" < "cat"){
+    alert("car is smaller than cat");
+    }
+
+*/
+
+
+    /*var EnterSubject1 = prompt("Enter  Subject  1");
+    var EnterSubject2 = prompt("Enter Subject 2");
+    var EnterSubject3 = prompt("Enter Subject 3");
+    var EnterNumber1 = +prompt("Enter Number Subject 1");
+    var EnterNumber2 = +prompt("Enter Number Subject 2");
+    var EnterNumber3 = +prompt("Enter Number Subject 3");
+    var PersentAge="Percentage";
+    var Grade="Garde";
+    var Remarsk4="Remarks";
+    var totalMarsks=EnterNumber1+EnterNumber2+EnterNumber3;
+    var per=totalMarsks/+300;
+     
+   document.write("<h3>"+PersentAge+"&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp"+Grade+"&nbsp&nbsp&nbsp"+Remarsk4+"<br>"+"<h3>"+"<br>");
+   
+   
+   var  Grade1 ="Greater Than or Equal to 80";
+   var  Grade2 ="Greater Than or Equal to 70";
+   var  Grade3 ="Greater Than or Equal to 60";
+   var  Grade4 ="Less Than  60";
+   var Remarsk ="Excellent";
+   var Remarsk2 ="Good";
+   var Remarsk2 ="You are Need To Improve";
+   var Remarsk3 ="You are Fail";
+   //document.write(Grade1+"<br>"+" "+"<br>"+Grade2+"<br>"+""+Grade3);
+    if (EnterNumber1 >=80) {
+        document.write(Grade1+" "+"A-One"+"&nbsp&nbsp&nbsp"+"Excillant"+"<br>");
+       
+    if(EnterNumber2 >=70){
+            document.write(Grade2+" "+"A"+"&nbsp&nbsp&nbsp"+"Good"+"<br>");
+        }
+
+        if (EnterNumber3 >=60){
+
+            document.write(Grade3+" "+"B"+"&nbsp&nbsp&nbsp"+"You need To improve"+"<br>");
+        }
+        
+    
+    }else 
+    {
+
+        document.write(Grade4+" "+"Fail"+"&nbsp&nbsp&nbsp"+"Sorry"+"<br>");
+
+    } 
+        
+
+  
+
+
+var InetCharacter =prompt('Enter character number or Nunber')
+var im=InetCharacter.toLowerCase()
+var iming=InetCharacter.toUpperCase()
+
+if(InetCharacter=== im){
+
+alert("Character is lower case")
+
+}else if(InetCharacter === iming){
+
+alert("Charatehr is upper case")
+
+}
+
+    
+chep end 1 to 20*/
+
+
+//Chapter#21to38
+
+
+
+
+//  Q1
+
+/*var FirstName =prompt("Enter First Name");
+var LastName=prompt("Last Name");
+
+var FullName = FirstName+" "+LastName;
+document.write("Wellcome"+" "+FullName);*/
+//Q2
+/*var EnterTheMobleName=prompt("Enter Your Mobile Name");
+var check=EnterTheMobleName.length;
+document.write(check);*/
+//Q3
+/*var CountryName ="Pakistan";
+
+var ind =CountryName.indexOf("n");
+
+document.write(ind);*/
+
+//Q4
+/*
+var HELLO ="HELLOW WORLD";
+var ind = HELLO.lastIndexOf("D");
+document.write(ind);
+*/
+
+/*Q5
+
+/*var CountryName ="Pakistan";
+var ind = CountryName.indexOf("i");
+document.write(ind);*/
+
+/*Q6
+
+/*var FirstName =prompt("Enter First Name");
+var LastName=prompt("Last Name");
+var res=FirstName.concat(LastName);
+document.write("Wellcome"+res);*/
+
+/*Q7*/
+/*var cont = "HYDERABAD";
+var res = cont.replace("HYDER","ISLAM");
+document.write(res);*/
+
+/*Q8*/
+
+/*var message="Ali and Sami are best friend.They play cricket and football togather";
+var rges = message.replace(/and/g,"&");
+document.write(rges);*/
+/*Q9*/
+/*
+var program ="472";
+var integer = parseInt(program);
+document.write(integer);*/
+
+/*Q10*/
+/*
+var Inter = prompt("Enter Your Name");
+var uper=Inter.toUpperCase();
+document.write(uper);*/
+
+
+/*Q11*/
+
+/*var Inter = prompt("Enter Your Name");
+var uper=Inte.charat
+document.write(uper);*/
+
+
+/*Q12*/
+
+/* var d = 36.36;
+ var s = d + '';
+ s =s.replace('.', '');
+ s = parseInt(s);
+ document.write(s);*/
+
+/*q13
+ /*var special = prompt('Enter uaename','User Name');
+ if(special === '@'||','||'!'){
+ 
+ alert('Special Character are note allowed');
+ 
+ }*/
+
+ /*Q14*/
+ /*var user=prompt("Enter User Name");
+ if(user === "@"||"."||","||"!"){
+
+alert("Enter correct User Name")
+
+
+ }else{
+
+    document.write(user);
+ }*/
+
+ /*Q14*/
+/*
+ var userinput=prompt("Enter Name ")
+ userinput=userinput.toLowerCase();
+ var bakery=["cake","apple","cookies","chips"]
+ for( var i=0;i < bakery.length;i++){
+
+    if(bakery[i] === userinput){
+
+        alert(userinput+" "+"are available")
+    }else{
+
+        document.write(userinput+" "+"note available");
+
+ }
+}
+*/
+/*15
+var pass= prompt('Enter Password','Enter Password')
+var paswd=  /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/;
+if(pass.value.match(paswd)){ 
+
+alert('Correct, try another...')
+
+}
+else
+{ 
+alert('Wrong...!')
+
+}
+  */
+ 
+ /* var uni = "University oF Karachi";
+ var str=uni.split("");
+document.write(str);
+
+*/
+
+//*25end*/
+
+
+
+/*26Start*/
+/*Q1/
+/*
+var num =+3.245+"<br>";
+var num2=Math.round(3.245)+"<br>";
+var num3=Math.floor(3.245)+"<br>";
+var num4=Math.ceil(3.245)+"<br>"; 
+document.write(num,num2,num3,num4);
+*/
+
+/*Q2*/
+/*var num =+-2.673+"<br>";
+var num2=Math.round(-2.673)+"<br>";
+var num3=Math.floor(-2.673)+"<br>";
+var num4=Math.ceil(-2.673)+"<br>"; 
+document.write(num,num2,num3,num4);*/
+
+/*Q3*/
+
+/*var num=Math.abs(-4)+"<br>";
+var num1=Math.abs(-5);
+document.write(num,num1);
+*/
+
+/*Q4*/
+
+/*var ren=Math.floor(Math.random()* 5 + 1);
+document.write("Dice value is"+ren);
+*/
+
+/*Q5*/
+
+/*var coin=Math.floor(Math.random()* 2+1);
+if(coin === 1){
+
+document.write("Rendom coin value Head"+" "+coin);
+
+}else{
+
+
+    document.write("Rendom coin value tails"+" "+coin);
+
+}*/
+
+
+
+
+
+
+/*Q6*/
+/*var ren=Math.floor(Math.random()*100+1);
+document.write("Random value between 1 and 100"+" ",ren);
+*/
+/*Q7*/
+
+/*var entrWeight=prompt("Enter Your Weight");
+var convert=entrWeight*100/100;
+document.write(convert);*/
+
+/*Q7*/
+/*var EntarNumber=prompt("Entar your Number")
+
+var luckyNumber = Math.floor(Math.random()*7+1)
+
+if(EntarNumber === luckyNumber)
+{
+
+Document.write("Congeratulatio you got lucky Number"+luckyNumber);
+    
+}else{
+
+
+document.write("Sorry this number are not lucky",luckyNumber)
+
+}end30*/
+
+/*31Start*/
+
+/*Q1*/
+/*var dt = new Date();
+document.write(dt);
+*/
+/*Q2*/
+/*
+var d = new Date();
+var month = new Array();
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+var n = month[d.getMonth()];
+
+document.write(n);
+
+*/
+/*Q3*/
+
+/*var d = new Date();
+var day = new Array();
+day[0] = "Mon";
+day[1] = "Tue";
+day[2] = "wed";
+day[3] = "The";
+day[4] = "Fri";
+day[5] = "Sat";
+day[6] = "Sun";
+var n = day[d.getDay()];
+alert("Today is"+" "+day[5]);
+*/
+
+/*var d = new Date();
+var day = new Array();
+day[0] = "Mon";
+day[1] = "Tue";
+day[2] = "wed";
+day[3] = "The";
+day[4] = "Fri";
+day[5] = "Sat";
+day[6] = "Sun";
+var n = day[d.getDay()];
+if(day ==="sat"||"sun")
+{
+
+document.write("Today is "+" "+day[5,6]+" "+"its a Fun day");
+
+}
+
+*/
+/*q5*/
+/*var d = new Date();
+
+if (d <=16){
+
+document.write(d+"First Fifteen days of themonth");
+
+}
+ */
+
+ /*Q6*/
+
+
+
+
+
+ /*Q7*/
+
+
+/*
+ var minutes = 1000 * 60;
+  var hours = minutes * 60;
+  var days = hours * 24;
+  var years = days * 365;
+  var d = new Date();
+  var t= d.getTime();
+
+  var y = Math.round(t / years)
+
+  document.write(y);*/
+
+  /*38*/
+
+  /*var DateOFBirtth = new Date(prompt("Enter Your Date Of Birth","Apr 26, 1989"))
+  var gtTiem=DateOFBirtth.getTime();
+  var today= new Date();
+  var todaymil=today.getTime();
+  var diff = todaymil-gtTiem;
+  var AccordDate=Math.floor(diff/(1000*60*60*24*30*12));
+  document.write("Your Till today is"+" "+AccordDate);*/
+  
+  
+/*Chepter35-38*/
+
+/*Q1*/
+/*
+function CurrntDate(d){
+ d = new Date();
+d.getTime();
+
+ document.write(d);
+
+}
+CurrntDate();*/
+
+/*Q2*/
+
+/*function greet(){
+
+    var  FirstName=prompt("Enteer Your First Name");
+    var LastName=prompt("Enteer Your Last Name");    
+     document.write("Well Come Mr."+" "+ FirstName,LastName);
+    
+
+}
+
+greet();*/
+
+/*Q3*/
+/*
+function add(){
+
+var num1 = +prompt("Enter First Number");
+var num2 = +prompt("Enter 2nd Number");
+var res=num1+num2;
+
+document.write("Total Is"+" "+res)
+
+}
+
+add();*/
+/*Q4*/
+
+/*function result(num1,num2,opp){
+num1=+prompt("Enter Num1");
+opp=prompt("Enter opprator");
+num2=+prompt("Enter Num2");
+if(opp === "+" ){
+    document.write(num1+num2);    
+    if(opp ==="-"){
+
+  
+        document.write(num1-num2);
+
+}
+    
+
+}
+
+
+}
+
+result();*/
+
+/*Q5*/
+/*var num = parseInt(prompt("Enter a number:"));
+function sumOfSquares(num) {
+    
+    var i;
+
+    for (i=0,i<=num,i++){
+        var sum=0;
+        var i = i*i;
+        sum = i;
+}
+     document.write("The sum of squares for numbers up to and including " +num+ "is " +sum+ ) ;
+}
+
+sumOfSquares(num);*/
+/*q6*/
+/*function factorial(n){
+    var answer = 1;
+    if (n == 0 || n == 1){
+      return answer;
+    }else{
+      for(var i = n; i >= 1; i--){
+        answer = answer * i;
+      }
+      return answer;
+    }  
+  }
+  let n = 4;
+  answer = factorial(n)
+  console.log("The factorial of " + n + " is " + answer);
+
+*/
+
+/*function myFunction(a,b) {
+    b= Math.pow(4, 3);
+  }
+  myFunction()
+  */
+ /*var side1 = a; 
+var side2 = b; 
+var side3 = c; 
+var s = (side1 + side2 + side3)/2;
+var area =  Math.sqrt(s*((s-side1)*(s-side2)*(s-side3)));
+console.log(area);*/
+
+/*function per(){
+
+var sub1= prompt('Sub1');
+var sub2= prompt('Sub2');
+var sub3= prompt('Sub3');
+
+var total=sub1+sub2+sub3;
+
+total/100;
+
+
+}
+*/
+/*
+var string = "heelloo world";
+var vowel = ["a", "e", "i", "o", "u"];
+
+String.prototype.character = function name() {
+    var i;
+    for ( i = 0; i < vowel.length; i++) {
+        var secondLoop = string.length;
+        for ( j = 0; j < secondLoop; j++) {
+            if (vowel[i] == string.charAt(j)) {
+                string = string.slice(0, j).concat(string.slice(j + 1, secondLoop));
+            }
+
+        }
+    }
+}
+
+string.character();
+console.log(string);*/
+
+/*42 end*/
+
+/*438 start*/
+/*Q1*/
+/*function myFunction() {
+    var x = document.createElement("IMG");
+    var y = document.createElement("IMG");
+    var z = document.createElement("IMG");
+    x.setAttribute("src", "1.jpg",);
+    x.setAttribute("width", "304");
+    x.setAttribute("height", "228");
+    document.body.appendChild(x);
+    y.setAttribute("src", "2.jpg",);
+    y.setAttribute("width", "304");
+    y.setAttribute("height", "228");
+    document.body.appendChild(y);
+    z.setAttribute("src", "2.jpg",);
+    z.setAttribute("width", "304");
+    z.setAttribute("height", "228");
+    document.body.appendChild(z);
+  }
+
+  alert("Click to show the picture"+myFunction())*/
+
+  /*Q2
+  function myFunction(){
+
+var rem=document.getElementById("myTable").deleteRow(1);
+
+  }*/
+
+/*Q4
+  function bigImg(x) {
+    x.style.height = "64px";
+    x.style.width = "64px";
+  }
+  
+  function normalImg(x) {
+    x.style.height = "32px";
+    x.style.width = "32px";
+  }*/
+
+  
+/*Q5
+
+  var addd = document.getElementById("add")
+   var count = 0;
+   function add() {
+  count += 1;
+  addd.innerHTML =  + count;
+}
+var SUBST = document.getElementById("add")
+var count = 0;
+ function SUB() {
+count += -1; 
+SUBST.innerHTML = count ;
+}*/
+/*49 to52*/
+/*Q1
+function signup(){
+
+var name=document.getElementById('name');
+var lastname=document.getElementById('pwd');
+document.write(name.value+"<br>"+"<br>")
+document.write(lastname.value)
+}*/
+
+/*q2
+function more(){
+var  mor = ['Banana'+"<br>",'Apple'+"<br>",'Mango'+"<br>",'Orange'];
+for(i=0;i<=mor.length;i++){
+document.write(mor[i]);
+
+
+}
+
+
+
+}*/
+
+  /*Q3
+  function myFunction(){
+
+var rem=document.getElementById("myTable").deleteRow(1);
+var rem=document.getElementById("myTable").EditeRow(1);
+
+  }*/
+
+  /*DOM
+var get = document.getElementById('main-content')
+var btElement = document.getElementsByClassName('main-content')
+var AlElemnet = document.getElementsByClassName('render')
+var anme = document.getElementById("first-name").value = "My value";
+*/
